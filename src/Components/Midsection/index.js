ecbca94880beddbb629cae7d269e5816044a0f10
Shirishanelapogula <shirishanelapogula@gmail.com>
2023-08-23 17:15:38 +0530

    commit

---
+++ src/Components/Midsection/index.js
@@ -0,0 +1,16 @@
+import "./index.css"
+
+const Midsection=()=>{
+    return (
+    <div className="mid-main-container">
+    <div className="top-container">
+        <h1 className="heading">Start building with Astro today</h1>
+        <button type="button" className="get-started-btn">Get Started</button>
+    </div>
+        <img src="https://res.cloudinary.com/dqfmzzr5g/image/upload/v1692782559/Screenshot_2023-08-23_145224_smfc03.png" alt="" className="image"/>
+        
+    </div>
+    )
+}
+
+export default Midsection
